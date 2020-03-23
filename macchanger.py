151e27dcf388dcc6917e37e4d82cd3c2172ed426
mystichoca <57755879+mystichoca@users.noreply.github.com>
2020-03-23 17:45:44 +0000

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -1,8 +1,10 @@
 import subprocess
 print("Mac Adresi Değiştiriliyor")
-interface=input("Interface Adı:   ")
+print("Changing Mac Address")
+interface=input("Interface Name (wlan0 by default):   ") #wifi card to be changed.
 subprocess.call(["ifconfig",interface,"down"])
-yeni_mac=input("Yeni MAC Adresi: ")
+yeni_mac=input("Yeni MAC Adresi (Enter New Mac): ")
 subprocess.call(["ifconfig",interface,"hw","ether",yeni_mac])
 subprocess.call(["ifconfig",interface,"up"])
 print("İşlem Tamam")
+print("Successfully Done")
